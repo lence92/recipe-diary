3a9a92fbaa910ac5864444fb1282c7b53968281c
Lenche Petkovska <lenche.petkovska.icloud.com>
2023-12-03 16:11:33 +0100

    recipe list css with tailwind

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ const initialRecipes = [
   {
     id: 1,
     name: "Franch Omlate",
+    source:
+      "https://media.istockphoto.com/id/478248524/photo/omelette-garnished-with-a-twig-of-marjoram.jpg?s=1024x1024&w=is&k=20&c=LfNd0OyB0hQ7Z825xzqmGavtOpGxwtRQc9jxHueZGck=",
     ingredients: ["2 eggs", "20gr moccarela cheese", "20gr mashrooms"],
+    time: "15 min",
     directions:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit." +
       "Suspendisse dictum lacus in est rutrum dictum. " +
@@ -21,12 +24,15 @@ const initialRecipes = [
   {
     id: 2,
     name: "Pancaks",
+    source:
+      "https://plus.unsplash.com/premium_photo-1672846027109-e2c91500afef?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     ingredients: [
       "2 eggs",
       "150gr flower",
       "10gr bakeing powder",
       "1 cup mineral water",
     ],
+    time: "30 min",
     directions:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
       "Vestibulum porttitor pharetra egestas. Aenean commodo at eros vitae feugiat. " +
@@ -56,11 +62,11 @@ export default function App() {
   }
 
   return (
-    <div className="recipe-diary">
+    <div className="m-4">
       <div>
         <RecipeList recipes={recipes} onOpenRecipe={handleOpenRecipe} />
 
-        <button className="toggleFormBtn" onClick={handleShowForm}>
+        <button onClick={handleShowForm}>
           {showForm ? "Close" : "Add recipe"}
         </button>
       </div>
--- src/RecipeList.js
+++ src/RecipeList.js
@@ -2,14 +2,32 @@ import Recipe from "./Recipe";
 
 export default function RecipeList({ recipes, onOpenRecipe }) {
   return (
-    <ul className="recipe-list">
+    <ul className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8">
       {recipes.map((recipe) => (
         <li
-          className="recipe"
           key={recipe.id}
+          className="relative"
           onClick={() => onOpenRecipe(recipe)}
         >
-          {recipe.name}
+          <div className="group aspect-h-7 aspect-w-10 block w-full overflow-hidden rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100">
+            <img
+              src={recipe.source}
+              alt=""
+              className="pointer-events-none object-cover group-hover:opacity-75"
+            />
+            <button
+              type="button"
+              className="absolute inset-0 focus:outline-none"
+            >
+              <span className="sr-only">View details for {recipe.name}</span>
+            </button>
+          </div>
+          <p className="pointer-events-none mt-2 block truncate text-sm font-medium text-gray-900">
+            {recipe.name}
+          </p>
+          <p className="pointer-events-none block text-sm font-medium text-gray-500">
+            {recipe.time}
+          </p>
         </li>
       ))}
     </ul>
